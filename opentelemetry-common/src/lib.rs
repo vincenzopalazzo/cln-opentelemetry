f52ae8996c8b103a5fd82bb9c7b6bdbb69430106
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-02-15 12:17:41 +0100

    common: implement the otp exported
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- opentelemetry-common/src/lib.rs
+++ opentelemetry-common/src/lib.rs
@@ -1,25 +1,37 @@
 pub mod log;
 pub use anyhow;
-use opentelemetry_prometheus::ExporterBuilder;
-use opentelemetry_sdk::export::logs::LogExporter;
 
-pub enum Exporter {
-    PROMETHEUS,
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::logs;
+use opentelemetry_sdk::runtime;
+use opentelemetry_sdk::Resource;
+
+pub struct Opentelemetry {
+    _logger: logs::Logger,
 }
 
-pub struct Opentelemetry<E: LogExporter + Clone + 'static> {
-    exporter_log: Option<E>,
-    exporter_metric: ExporterBuilder,
-}
+impl Opentelemetry {
+    pub fn new(tag: &str, exporter_endpoint: &str) -> anyhow::Result<Self> {
+        let logger = opentelemetry_otlp::new_pipeline()
+            .logging()
+            .with_log_config(logs::Config::default().with_resource(Resource::new(vec![
+                KeyValue::new(
+                    opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                    tag.to_owned(),
+                ),
+            ])))
+            .with_exporter(
+                opentelemetry_otlp::new_exporter()
+                    .tonic()
+                    .with_endpoint(exporter_endpoint),
+            )
+            .install_batch(runtime::Tokio)?;
+        Ok(Self { _logger: logger })
+    }
 
-impl<E: LogExporter + Clone + 'static> Opentelemetry<E> {
-    pub fn new(exporter_metric: Exporter, exporter_log: E) -> anyhow::Result<Self> {
-        let exporter = match exporter_metric {
-            Exporter::PROMETHEUS => opentelemetry_prometheus::exporter(),
-        };
-        Ok(Self {
-            exporter_log: None,
-            exporter_metric: exporter,
-        })
+    pub fn init_log(&self, level: &str) -> anyhow::Result<()> {
+        log::init(level)?;
+        Ok(())
     }
 }
--- opentelemetry-common/src/log.rs
+++ opentelemetry-common/src/log.rs
@@ -1,25 +1,16 @@
 //! Logging module.
-//!
-//! Credit to https://github.com/vincenzopalazzo/nakamoto/blob/master/node/src/logger.rs
 use std::str::FromStr;
 
-use log::Level;
+use opentelemetry::global::logger_provider;
 use opentelemetry_appender_log::OpenTelemetryLogBridge;
-use opentelemetry_sdk::export::logs::LogExporter;
-use opentelemetry_sdk::logs::{BatchLogProcessor, LoggerProvider};
-use opentelemetry_sdk::runtime;
 
-/// Initialize a new logger.
-pub fn init<E: LogExporter + Clone + 'static>(level: &str, exporter: E) -> anyhow::Result<()> {
-    let level = Level::from_str(level).map_err(|err| anyhow::anyhow!("{err}"))?;
-
-    //Create a LoggerProvider and register the exporter
-    let logger_provider = LoggerProvider::builder()
-        .with_log_processor(BatchLogProcessor::builder(exporter.clone(), runtime::Tokio).build())
-        .build();
+/// Initialize a new logger exported with open telemetry.
+pub fn init(level: &str) -> anyhow::Result<()> {
+    let logger_provider = logger_provider();
 
     let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
     log::set_boxed_logger(Box::new(otel_log_appender)).map_err(|err| anyhow::anyhow!("{err}"))?;
+    let level = log::Level::from_str(level)?;
     log::set_max_level(level.to_level_filter());
 
     Ok(())

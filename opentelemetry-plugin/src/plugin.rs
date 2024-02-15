0a51555f443947e69901897f2596f213791e91bb
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-02-15 12:31:16 +0100

    wip: configure the plugin and start collects logs
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- opentelemetry-plugin/src/plugin.rs
+++ opentelemetry-plugin/src/plugin.rs
@@ -1,7 +1,9 @@
 //! Plugin implementation
 //!
 //! Author: Vincenzo Palazzo <vincenzopalazzo@member.fsf.org>
-use serde_json::Value;
+use json::Value;
+use opentelemetry_common::Opentelemetry;
+use serde_json as json;
 
 use clightningrpc_plugin::commands::RPCCommand;
 use clightningrpc_plugin::plugin::Plugin;
@@ -17,7 +19,7 @@ impl State {
 }
 
 pub fn build_plugin() -> anyhow::Result<Plugin<State>> {
-    let plugin = plugin! {
+    let mut plugin = plugin! {
         state: State::new(),
         dynamic: true,
         notification: [
@@ -26,6 +28,16 @@ pub fn build_plugin() -> anyhow::Result<Plugin<State>> {
         methods: [],
         hooks: [],
     };
+    plugin.on_init(|_| {
+        // FIXME: take from plugin the target + endpoint + log level (I think I can get it from the configuration)
+        let resp = Opentelemetry::new("test", "")
+            .and_then(|val| val.init_log("info"))
+            .ok()
+            .map(|_| json::json!({ "disable": "Disabling due a open telemetry init error" }));
+        // SAFETY: this is always some, is there is not error, is
+        // Some(()) otherwise is Some({ disable: ... })
+        resp.unwrap()
+    });
     Ok(plugin)
 }
 
